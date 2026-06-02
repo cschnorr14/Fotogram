b0b2cb39c53c7fcd342f83b1daa7e57a381a0db8
CHristina <christina.schnorr@gmx.de>
2026-06-02 14:26:15 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,481 @@
+
+//file names of the pictures
+const myImgsBike = [
+    "IMG_2355.jpeg",
+    "IMG_1136.jpeg",
+    "IMG_1638.jpeg",
+    "IMG_1886.jpeg",
+    "IMG_1242.jpeg",
+    "IMG_1723.jpeg",
+    "IMG_1969.jpeg",
+    "IMG_9257.jpeg",
+    "IMG_1058.jpeg",
+    "IMG_1401.jpeg",
+    "IMG_1122.jpeg",
+    "IMG_1605.jpeg",
+    "IMG_1880.jpeg",
+    "IMG_2351.jpeg"
+];
+
+const myImgsTanzania = [
+    "celebrate-1730060565668.jpeg",	
+    "celebrate-1730096920257.jpeg",
+    "IMG_0620.jpeg",
+     "IMG_0572.jpeg",
+    "celebrate-1730097046143.jpeg",
+    "IMG_0716.jpeg",
+    "celebrate-1730098165455.jpeg",
+    "celebrate-1730098570060.jpeg",
+    "celebrate-1730093496195.jpeg",	
+    "celebrate-1731441605257.jpeg"
+];
+
+const myImgsLaReunion = [
+    "IMG_6245.jpeg",
+    "IMG_6652.jpeg",
+    "IMG_6718.jpeg",
+    "IMG_7370.jpeg",
+    "IMG_7395.jpeg"
+];
+
+const myImgsPatagonia = [
+    "Patagonien.jpeg",
+    "IMG_9818.jpg",
+    "IMG_9820.jpg",
+    "IMG_9839.jpg",
+    "ushuaia.jpg",
+    "IMG_9856.jpeg",
+    "IMG_9972.jpeg",
+    "IMG_9338.jpeg",
+    "IMG_9114.jpeg",
+    "IMG_9190.jpeg",
+    "IMG_9418.jpeg",
+    "IMG_9420.jpeg",
+    "IMG_9018.jpeg",
+    "20250216_201147.jpeg",
+    "20250217_060126.jpeg"
+];
+
+const myImgsAntarctica = [
+    "Antarktis.jpeg",
+    "Wildlife.jpg",
+    "IMG_1595.jpeg",
+    "IMG_1094.jpeg",
+    "20250224_195225.jpeg",
+    "IMG_0673.jpeg",
+    "IMG_1121.jpeg",
+    "IMG_1129.jpeg",
+    "IMG_1135.jpeg",
+    "IMG_1228.jpeg",
+    "IMG_1595.jpeg",
+    "20250223_111444.jpeg",
+    "IMG_0730.jpeg",
+    "20250224_161534.jpeg"
+];
+
+const myImgsArgentina = [
+    "Argentina.jpeg",
+    "IMG_2346.jpeg",
+    "IMG_2422.jpeg",
+    "IMG_2436.jpeg",
+    "IMG_2481.jpeg",
+    "IMG_2577.jpeg"
+];
+
+const myImgsBrasil = [
+    "Rio.jpeg",
+    "20250318_173324.jpeg",
+    "20250318_174119.jpeg",
+    "20250321_170000.jpeg",
+    "20250321_172355.jpeg",
+    "20250324_151204.jpeg",
+    "20250325_124450.jpeg",
+    "20250325_131300.jpeg",
+    "20250325_170440.jpeg", 
+    "20250326_170504.jpeg",
+    "20250326_180030(0).jpeg",
+    "sunset.jpeg",
+    "IMG_2802.jpeg",
+    "IMG_2848.jpeg",
+    "IMG_2853.jpeg",
+    "IMG_2891.jpeg",
+    "IMG_3222.jpeg",
+    "IMG_3628.jpeg",
+    "sugarloaf.jpeg"
+];
+
+const myImgsGalapagos = [
+    "Galapagos.jpeg",
+    "20250401_150047.jpeg",
+    "IMG_4112.jpeg",
+    "IMG_4951.jpeg",
+    "20250414_175246.jpeg",
+    "IMG_4371.jpeg",
+    "IMG_5243.jpeg",
+    "GOPR9030.jpeg",
+    "IMG_4510.jpeg",
+    "IMG_4574.jpeg",
+    "grafitti.jpeg",
+    "IMG_3965.jpeg"
+];
+
+const myImgsAtacama = [
+    "Salar de Uyuni.jpeg",
+    "20250424_162610.jpeg",
+    "20250424_165041.jpeg",
+    "20250430_131444.jpeg",
+    "20250426_160803.jpeg",
+    "IMG_5430.jpeg",
+    "20250427_080610.jpeg",
+    "IMG_5601.jpeg",
+    "20250428_105827.jpeg",
+    "IMG_5615.jpeg",
+    "20250428_183207.jpeg",
+    "IMG_6045.jpeg",
+    "20250429_155126.jpeg",		
+    "20250429_162828.jpeg",		
+    "IMG_6149.jpeg",
+    "20250429_172614.jpeg",		
+    "IMG_6180.jpeg",
+    "20250429_181042.jpeg",		
+    "IMG_6368.jpeg",
+    "20250430_082428_005_saved.jpeg",	
+    "IMG_6484.jpeg"
+];
+
+const myImgsBolivia = [
+    "Bolivien.jpeg",
+    "IMG_7302.jpeg",
+    "IMG_6673.jpeg",
+    "IMG_6676.jpeg",
+    "IMG_7481.jpeg",
+    "IMG_7525.jpeg",
+    "IMG_7735.jpeg",
+    "IMG_6675.jpeg",
+    "IMG_7530.jpeg",
+    "IMG_7232.jpeg",
+    "IMG_7739.jpeg"
+];
+
+const myImgsPeru = [
+    "Peru.jpeg",
+    "20250527_135519.jpeg",
+    "20250629_093114.jpeg",
+    "IMG_8232.jpeg",
+    "20250527_141525.jpeg",
+    "20250630_072852.jpeg",
+    "20250621_133238.jpeg",
+    "IMG_7799.jpeg",
+    "IMG_9517.jpeg",
+    "20250622_135524.jpeg",
+    "IMG_7967.jpeg",
+    "IMG_9575.jpeg",
+    "20250622_143210.jpeg",
+    "IMG_8094.jpeg",
+    "IMG_9915.jpeg"
+];
+
+const myImgsPanama = [
+    "Panama.jpeg",
+    "IMG_3083.jpeg",
+    "IMG_3524.jpeg",
+    "IMG_3608.jpeg",
+    "IMG_3473.jpeg",	
+    "IMG_3590.jpeg",	
+    "IMG_4061.jpeg"
+];
+
+const myImgsTaiwan = [
+    "Taiwan.jpeg",
+    "IMG_5573.jpeg",
+    "IMG_5621.jpeg",
+    "IMG_5717.jpeg",
+    "IMG_5792.jpeg"
+];
+
+const myImgsKanaren = [
+    "IMG_6853.jpeg",
+    "IMG_6761.jpeg",
+    "IMG_6835.jpeg",
+    "IMG_6912.jpeg",
+    "IMG_7195.jpeg",
+    "IMG_4309.jpeg",
+    "IMG_6610.jpeg",
+    "IMG_6794.jpeg",
+    "IMG_6853.jpeg",
+    "IMG_6934.jpeg",
+    "IMG_7197.jpeg",
+    "IMG_6703.jpeg",
+    "IMG_6799.jpeg",
+    "IMG_6903.jpeg",
+    "IMG_6972.jpeg"
+];
+
+const myImgsMarokko = [
+    "Marokko.jpg",
+    "IMG_8560.jpeg",
+    "IMG_8434.jpeg",
+    "IMG_8594.jpeg",
+    "IMG_8900.jpeg",
+    "IMG_8469.jpeg",
+    "IMG_8593.jpeg",
+    "IMG_8702.jpeg"
+];
+
+const albumTitle = [
+    "Alpenüberquerung",
+    "Tansania",
+    "La Réunion",
+    "Patagonien",
+    "Antarktis",
+    "Argentinien",
+    "Rio de Janeiro",
+    "Galapagos",
+    "Atacama & Salar de Uyuni",
+    "Bolivien",
+    "Peru",
+    "Panama",
+    "Taiwan",
+    "Kanaren",
+    "Marokko"
+];
+
+//function to open dialog
+const dialogRef = document.getElementById("dialog");
+function openDialog() {
+    dialogRef.showModal();
+    dialogRef.classList.add("opened");
+    //renderFiltered(i); = Option 2 -> wrapper function, but how is it defined depending on album?
+    //renderFiltered is triggered via onclick together with openDialog()
+    /*dialogRef.stopPropagation();*/
+}
+
+//function to decide which pictures are rendered in dialog
+let currentPictures = []; //empty array -> to be filled with selected album
+
+let currentIndex = 0; //indicates which picture is recently shown
+
+let currentTitle = []; //empty array -> to be filled with album titles
+
+const nameAlbumTitle = document.getElementById("dialogTitle");
+
+
+function renderFiltered(i) {
+    if (i == 1) {
+        currentPictures = myImgsBike;
+        currentTitle = albumTitle[0];   //adding title to photo album
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 2) {
+        currentPictures = myImgsTanzania;
+        currentTitle = albumTitle[1];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 3) {
+        currentPictures = myImgsLaReunion;
+        currentTitle = albumTitle[2];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+    
+    if (i == 4) {
+        currentPictures = myImgsPatagonia;
+        currentTitle = albumTitle[3];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+
+    }
+
+    if (i == 5) {
+        currentPictures = myImgsAntarctica;
+        currentTitle = albumTitle[4];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 6) {
+        currentPictures = myImgsArgentina;
+        currentTitle = albumTitle[5];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 7) {
+        currentPictures = myImgsBrasil;
+        currentTitle = albumTitle[6];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 8) {
+        currentPictures = myImgsGalapagos;
+        currentTitle = albumTitle[7];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 9) {
+        currentPictures = myImgsAtacama;
+        currentTitle = albumTitle[8];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 10) {
+        currentPictures = myImgsBolivia;
+        currentTitle = albumTitle[9];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 11) {
+        currentPictures = myImgsPeru;
+        currentTitle = albumTitle[10];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 12) {
+        currentPictures = myImgsPanama;
+        currentTitle = albumTitle[11];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 13) {
+        currentPictures = myImgsTaiwan;
+        currentTitle = albumTitle[12];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 14) {
+        currentPictures = myImgsKanaren;
+        currentTitle = albumTitle[13];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    if (i == 15) {
+        currentPictures = myImgsMarokko;
+        currentTitle = albumTitle[14];
+        nameAlbumTitle.innerHTML = `${currentTitle}`;
+
+    }
+
+    currentIndex = 0; 
+    render();
+}
+
+
+//function to load pictures of a certain album/array into dialog
+const dialogIMG = document.getElementById("dialogIMG");
+
+const amountPictures = document.getElementById("amountPics");
+
+function render() {
+    /*let contentRef = document.getElementById("dialog");
+    dialogIMG = currentPictures[0];*/
+    
+    if (currentPictures.length > 0) {
+        dialogIMG.src = "img/final/" + currentPictures[currentIndex];
+
+        //adapt picture number
+        const currentPicNumber = currentIndex + 1;
+        const totalPicNumber = currentPictures.length;
+        amountPictures.innerHTML = `${currentPicNumber} / ${totalPicNumber}`;
+
+    }
+}
+
+//old stuff:
+/*contentRef.innerHTML = ""; //empties array at the beginning of for-loop
+for (let index=0; index < currentPictures.length; index++){
+
+    contentRef.innerHTML += myImgsPatagonia;
+    //getNoteTemplate(index, currentPictures);
+
+}
+}
+
+function getNoteTemplate(index){
+return `
+`
+}*/
+
+
+//function to close dialog
+function closeDialog() {
+    dialogRef.close();
+    dialogRef.classList.remove("opened");
+}
+
+
+//closes dialog if clicked on background:
+
+dialog.addEventListener('click', (event) => {
+     const rect = dialog.getBoundingClientRect(); //measures exact size of dialog by saving the x/y-coordinates of the 4 dialog edges in pixel
+    //rect.left & rect.right = where left edge starts & right edge of dialog box ends
+    //rect.top & rect.bottom = where upper edge starts & lower edge ends of the dialog box
+    //event.clickX/Y uses x/y-coordinates of mouse click
+
+     const isInDialog = (               //boolean which tests if click is within dialog
+    event.clientX >= rect.left &&       //tests if the click is right from the left edge?
+    event.clientX <= rect.right &&      //tests if the click is left from the right edge?
+    event.clientY >= rect.top &&        //tests if the click is below the upper edge?
+    event.clientY <= rect.bottom        //tests if the click is higher than the lower edge?
+  );
+
+  if(!isInDialog){
+    closeDialog();
+  }
+});
+
+//old version to close dialog if clicked on background, but didn't work because of padding
+  /*
+  dialog.addEventListener('click', (event) => {
+  if (event.target === dialog){
+
+    closeDialog();}
+    //event.stopPropagation();
+});*/
+
+//closes dialog when escape is pressed:
+dialog.addEventListener('close', () => {
+    closeDialog();
+});
+
+//functions to scroll through photo album
+function goBack() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        render();
+    } else {
+        currentIndex = currentPictures.length - 1; // sets for-loop at the end
+        render();
+    }
+}
+
+function goForward() {
+    if (currentIndex < currentPictures.length - 1) {
+        currentIndex++;
+        render();
+    } else {
+        currentIndex = 0; // sets for-loop at beginning
+        render();
+    }
+
+}
+
+//option to scroll through album via keyboard (arrow-keys):
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      goForward();
+    } else if (event.key === 'ArrowLeft') {
+      goBack();
+    }
+});
